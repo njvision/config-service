87e0413638f4e1326c14b520893f3ce02d49f12a
njvision <natjel1982@gmail.com>
2023-04-04 22:32:51 +0300

    add unit test

--- src/test/java/com/mdbookshop/configservice/ConfigServiceApplicationTests.java
+++ src/test/java/com/mdbookshop/configservice/ConfigServiceApplicationTests.java
@@ -1,13 +1,27 @@
 package com.mdbookshop.configservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ConfigServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
+	@Test
+	@Disabled("You can enable this test if your config-repo is publicly available")
+	void defaultConfigurationAvailable() {
+
+		ResponseEntity<Environment> entity = testRestTemplate.getForEntity("/application/default", Environment.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 }
